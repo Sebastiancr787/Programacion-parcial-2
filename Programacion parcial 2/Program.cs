f2766775a67e740e597dee0dc199272c35db7b0a
Sebastian Castrillon Ruiz <sebastian.castrillonr@upb.edu.co>
2021-10-22 16:58:43 -0500

    Pruebas menu

--- Programacion parcial 2/Program.cs
+++ Programacion parcial 2/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Chao");
+﻿Console.WriteLine("Hola, bienvenido a la tienda el lago, elige una opcion\n" +
+                 "\n1.- Buscar producto " +
+                 "\n2.- Suma de productos " +
+                 "\n3.- pagar cuenta " +
+                 "\n4.- Consultar cuenta cliente" +
+                 "\n5.- Actualizar cuenta cliente " +
+                 "\n6.- Calcular informe de ventas " +
+                 "\n7.- Calcular cartera de clientes " +
+                 "\n8.- Acerca de  " +
+                 "\n9.- Salir ");
+
+int i1 = 0;
+String s1 = null;
+
+i1 = Convert.ToInt16(Console.ReadLine());
+
